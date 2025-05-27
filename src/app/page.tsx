b0e19ec18ec09aba23bbd3b548e59243b743c2bc
aaronBIOO <aaronnabioo@gmail.com>
2025-05-27 21:28:05 +0000

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <h1 className="text-4xl font-bold mb-8">
+        <h1 className="text-4xl font-md mb-8">
           Welcome to your Social App!
         </h1>
         {/* Render the Client Component here. 
