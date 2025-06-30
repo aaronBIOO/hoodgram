4d10a8a745d6e13300f6f7f5945cf46cdc090bbb
aaronBIOO <aaronnabioo@gmail.com>
2025-06-30 22:30:54 +0000

    update

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@ export default function SignInPage() {
   return (
     <div>
       <h1>Sign In Page</h1>
-      <p>This is where your SigninForm component will go.</p>
+      <p>This is where your SigninForm component will go</p>
     </div>
   );
 }
